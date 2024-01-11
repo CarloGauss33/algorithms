f869447ecf9447c9dd194113f44bd58837fc1621
Carlos Paredes <carlos@paredesrobles.com>
2024-01-10 23:10:00 -0300

    wip(App): add PoC render

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,84 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { onMounted } from "vue";
+<script setup lang="ts">
+import { createCanvas, createCircle, connectFigures} from './utils/canvas'
+import {onMounted} from 'vue'
 
-onMounted(() => {
-  const script = document.createElement("script");
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
-  script.src = "https://buttons.github.io/buttons.js";
-  script.async = true;
+onMounted(async () => {
+  document.addEventListener('DOMContentLoaded', async () => {
+    const elem = document.getElementById('canvas');
 
-  document.body.appendChild(script);
-});
+    const two = createCanvas(elem as HTMLElement);
+
+    const boundingBox = two.renderer.domElement.getBoundingClientRect();
+    console.log(boundingBox);
+
+    const circleRadius = 50;
+
+    const nodesInTree = 8;
+
+    const circles = [];
+    const lines = [];
+
+    const insertionQueue = [];
+    
+    const root = createCircle(
+      two,
+      boundingBox.width / 2,
+      100,
+      circleRadius
+    );
+
+    console.log('root', root);
+
+    circles.push(root);
+    insertionQueue.push(root);
+
+    while (insertionQueue.length > 0) {
+      if (circles.length > nodesInTree - 1){
+        break;
+      }
+
+      const currentCircle = insertionQueue.shift();
+      console.log('currentCircle', currentCircle);
+      const position = currentCircle.translation;
+
+      await delay(700); // Add delay here
+
+      const leftChild = createCircle(
+        two,
+        position.x - circleRadius * (800 - position.y) / 100,
+        position.y + 200,
+        circleRadius
+      );
+      lines.push(connectFigures(two, currentCircle, leftChild));
+
+      await delay(700); // Add delay here
+
+      const rightChild = createCircle(
+        two,
+        position.x + circleRadius * (800 - position.y) / 100,
+        position.y + 200,
+        circleRadius
+      );
+      lines.push(connectFigures(two, currentCircle, rightChild));
+
+      circles.push(leftChild);
+      circles.push(rightChild);
+
+      insertionQueue.push(leftChild);
+      insertionQueue.push(rightChild);
+    
+    }
+
+    two.update(); // Update the canvas to reflect the new circles and lines.
+  })
+}) 
 </script>
 
 <template>
-  <div class="bg-gray-800 h-screen text-white">
-    <header class="py-16">
-      <h1 class="text-5xl font-bold text-center mb-6">Create Tailwind</h1>
-      <p className="text-center mb-6 text-xl">
-        If you like this project, consider giving it a star on GitHub!
-      </p>
-
-      <div class="flex flex-row justify-center items-center gap-4">
-        <a
-          class="github-button"
-          href="https://github.com/andrejjurkin/create-tw"
-          data-color-scheme="no-preference: dark; light: dark; dark: dark;"
-          data-icon="octicon-star"
-          data-size="large"
-          data-show-count="true"
-          aria-label="Star andrejjurkin/create-tw on GitHub"
-        >
-          Star
-        </a>
-        <a
-          class="github-button"
-          href="https://github.com/andrejjurkin/create-tw/discussions"
-          data-color-scheme="no-preference: dark; light: dark; dark: dark;"
-          data-icon="octicon-comment-discussion"
-          data-size="large"
-          aria-label="Discuss andrejjurkin/create-tw on GitHub"
-        >
-          Discuss
-        </a>
-      </div>
-    </header>
-
-    <div class="flex gap-16 items-center justify-center">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="w-24 h-24" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="w-24 h-24" alt="Vue logo" />
-      </a>
-    </div>
+  <div class="min-h-screen bg-gray-100 flex flex-col">
+    <div class="bg-blue-800 h-6"></div>
+    <div id="canvas" class="min-h-screen flex-grow"></div>
   </div>
 </template>
